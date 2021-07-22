1db46664c38119f1601494aff4ee4c0e630a9062
eaevanarnett <85958448+eaevanarnett@users.noreply.github.com>
2021-07-22 11:47:20 -0700

    Nested iteration within a loop. invoked function

--- JS/directory.js
+++ JS/directory.js
@@ -1,17 +1,17 @@
-const data = document.querySelector('.column');
-
-let html = '';
-
-for ( let i = 0; i < pets.length; i++ ) {
-    let pet = pets[i];
-    html += `
-        <h2>${pet.name}</h2>
-        <h3>${pet.type} | ${pet.breed}</h3>
-        <p> Age: ${pet.age}</p>
-        <img src='${pet.photo}' alt='${pet.breed}'>
-    `;
+const createDirectory = arr => {
+    const data = document.querySelector('.column');
+    let html = '';
+    for ( let i = 0; i < pets.length; i++ ) {
+        let pet = pets[i];
+        html += `
+            <h2>${pet.name}</h2>
+            <h3>${pet.type} | ${pet.breed}</h3>
+            <p> Age: ${pet.age}</p>
+            <img src='${pet.photo}' alt='${pet.breed}'>
+        `;
+    }
+    data.insertAdjacentHTML('beforeend', html);
 }
 
-// .column also referred to as node in DOM
-// insertAdjacentHTML(<string of where inside 'main' I am inserting HTML>, <actual HTML string>)
-data.insertAdjacentHTML('beforeend', html);
+createDirectory( pets );
+// insertAdjacentHTML(<string of where inside '<element/selector>' I am inserting HTML>, <actual HTML string>)
