7ac7c69672808c3b8460b130a85725142686b150
eaevanarnett <85958448+eaevanarnett@users.noreply.github.com>
2021-07-22 13:33:19 -0700

    Produced a return message based on a pre-made grocery list to determine if item is on list

--- JS/directory.js
+++ JS/directory.js
@@ -15,3 +15,27 @@ const createDirectory = arr => {
 
 createDirectory( pets );
 // insertAdjacentHTML(<string of where inside '<element/selector>' I am inserting HTML>, <actual HTML string>)
+
+
+const inStock = ['pizza', 'cookies', 'eggs', 'apples', 'milk', 'cheese', 'bread', 'lettuce', 'carrots', 'broccoli', 'potatoes', 'crackers', 'onions', 'tofu', 'limes', 'cucumbers'];
+
+const search = prompt('Search for a product.');
+let productIndex;
+let message;
+
+if ( search ) {
+    search = search.toLowerCase();
+    productIndex = inStock.indexOf(search);
+}
+
+if ( !search ) {
+    message = "Sorry please enter a valid item.";   
+} else if ( productIndex !== -1) {
+    message = `${search} are in stock! It's is item #${productIndex + 1} on the list:) `;
+} else {
+    message = `Sorry ${search} is not in stock. Check in next week. `;
+}
+
+message += `<h3>In Stock:</h3> <p>${inStock.join(", ")}.</p>`;
+
+document.querySelector('main').innerHTML = `<p>${message}</p>`;
